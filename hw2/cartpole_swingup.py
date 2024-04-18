cd1c07a1d2f5ebbb71302b9d69c6b6741aa1118c
dan <danielpmorton@gmail.com>
2024-04-18 14:59:43 -0700

    Delete files from previous years, move HW2 code to folder

--- hw2/cartpole_swingup.py
+++ hw2/cartpole_swingup.py
@@ -85,9 +85,9 @@ def ilqr(f, s0, s_goal, N, Q, R, QN, eps=1e-3, max_iters=1000):
         at time step `k`, for `k = 0, 1, ..., N-1`
     """
     if max_iters <= 1:
-        raise ValueError('Argument `max_iters` must be at least 1.')
-    n = Q.shape[0]        # state dimension
-    m = R.shape[0]        # control dimension
+        raise ValueError("Argument `max_iters` must be at least 1.")
+    n = Q.shape[0]  # state dimension
+    m = R.shape[0]  # control dimension
 
     # Initialize gains `Y` and offsets `y` for the policy
     Y = np.zeros((N, m, n))
@@ -99,7 +99,7 @@ def ilqr(f, s0, s_goal, N, Q, R, QN, eps=1e-3, max_iters=1000):
     s_bar = np.zeros((N + 1, n))
     s_bar[0] = s0
     for k in range(N):
-        s_bar[k+1] = f(s_bar[k], u_bar[k])
+        s_bar[k + 1] = f(s_bar[k], u_bar[k])
     ds = np.zeros((N + 1, n))
     du = np.zeros((N, m))
 
@@ -119,56 +119,59 @@ def ilqr(f, s0, s_goal, N, Q, R, QN, eps=1e-3, max_iters=1000):
             converged = True
             break
     if not converged:
-        raise RuntimeError('iLQR did not converge!')
+        raise RuntimeError("iLQR did not converge!")
     return s_bar, u_bar, Y, y
 
 
 def cartpole(s, u):
     """Compute the cart-pole state derivative."""
-    mp = 2.     # pendulum mass
-    mc = 10.    # cart mass
-    L = 1.      # pendulum length
-    g = 9.81    # gravitational acceleration
+    mp = 2.0  # pendulum mass
+    mc = 10.0  # cart mass
+    L = 1.0  # pendulum length
+    g = 9.81  # gravitational acceleration
 
     x, θ, dx, dθ = s
     sinθ, cosθ = jnp.sin(θ), jnp.cos(θ)
-    h = mc + mp*(sinθ**2)
-    ds = jnp.array([
-        dx,
-        dθ,
-        (mp*sinθ*(L*(dθ**2) + g*cosθ) + u[0]) / h,
-        -((mc + mp)*g*sinθ + mp*L*(dθ**2)*sinθ*cosθ + u[0]*cosθ) / (h*L)
-    ])
+    h = mc + mp * (sinθ**2)
+    ds = jnp.array(
+        [
+            dx,
+            dθ,
+            (mp * sinθ * (L * (dθ**2) + g * cosθ) + u[0]) / h,
+            -((mc + mp) * g * sinθ + mp * L * (dθ**2) * sinθ * cosθ + u[0] * cosθ)
+            / (h * L),
+        ]
+    )
     return ds
 
 
 # Define constants
-n = 4                                       # state dimension
-m = 1                                       # control dimension
-Q = np.diag(np.array([10., 10., 2., 2.]))   # state cost matrix
-R = 1e-2*np.eye(m)                          # control cost matrix
-QN = 1e2*np.eye(n)                          # terminal state cost matrix
-s0 = np.array([0., 0., 0., 0.])             # initial state
-s_goal = np.array([0., np.pi, 0., 0.])      # goal state
-T = 10.                                     # simulation time
-dt = 0.1                                    # sampling time
-animate = False                             # flag for animation
-closed_loop = False                         # flag for closed-loop control
+n = 4  # state dimension
+m = 1  # control dimension
+Q = np.diag(np.array([10.0, 10.0, 2.0, 2.0]))  # state cost matrix
+R = 1e-2 * np.eye(m)  # control cost matrix
+QN = 1e2 * np.eye(n)  # terminal state cost matrix
+s0 = np.array([0.0, 0.0, 0.0, 0.0])  # initial state
+s_goal = np.array([0.0, np.pi, 0.0, 0.0])  # goal state
+T = 10.0  # simulation time
+dt = 0.1  # sampling time
+animate = False  # flag for animation
+closed_loop = False  # flag for closed-loop control
 
 # Initialize continuous-time and discretized dynamics
 f = jax.jit(cartpole)
-fd = jax.jit(lambda s, u, dt=dt: s + dt*f(s, u))
+fd = jax.jit(lambda s, u, dt=dt: s + dt * f(s, u))
 
 # Compute the iLQR solution with the discretized dynamics
-print('Computing iLQR solution ... ', end='', flush=True)
+print("Computing iLQR solution ... ", end="", flush=True)
 start = time.time()
-t = np.arange(0., T, dt)
+t = np.arange(0.0, T, dt)
 N = t.size - 1
 s_bar, u_bar, Y, y = ilqr(fd, s0, s_goal, N, Q, R, QN)
-print('done! ({:.2f} s)'.format(time.time() - start), flush=True)
+print("done! ({:.2f} s)".format(time.time() - start), flush=True)
 
 # Simulate on the true continuous-time system
-print('Simulating ... ', end='', flush=True)
+print("Simulating ... ", end="", flush=True)
 start = time.time()
 s = np.zeros((N + 1, n))
 u = np.zeros((N, m))
@@ -178,35 +181,35 @@ for k in range(N):
     # INSTRUCTIONS: Compute either the closed-loop or open-loop value of
     # `u[k]`, depending on the Boolean flag `closed_loop`.
     if closed_loop:
-        u[k] = 0.
+        u[k] = 0.0
         raise NotImplementedError()
     else:  # do open-loop control
-        u[k] = 0.
+        u[k] = 0.0
         raise NotImplementedError()
     ###########################################################################
-    s[k+1] = odeint(lambda s, t: f(s, u[k]), s[k], t[k:k+2])[1]
-print('done! ({:.2f} s)'.format(time.time() - start), flush=True)
+    s[k + 1] = odeint(lambda s, t: f(s, u[k]), s[k], t[k : k + 2])[1]
+print("done! ({:.2f} s)".format(time.time() - start), flush=True)
 
 # Plot
 fig, axes = plt.subplots(1, n + m, dpi=150, figsize=(15, 2))
 plt.subplots_adjust(wspace=0.45)
-labels_s = (r'$x(t)$', r'$\theta(t)$', r'$\dot{x}(t)$', r'$\dot{\theta}(t)$')
-labels_u = (r'$u(t)$',)
+labels_s = (r"$x(t)$", r"$\theta(t)$", r"$\dot{x}(t)$", r"$\dot{\theta}(t)$")
+labels_u = (r"$u(t)$",)
 for i in range(n):
     axes[i].plot(t, s[:, i])
-    axes[i].set_xlabel(r'$t$')
+    axes[i].set_xlabel(r"$t$")
     axes[i].set_ylabel(labels_s[i])
 for i in range(m):
     axes[n + i].plot(t[:-1], u[:, i])
-    axes[n + i].set_xlabel(r'$t$')
+    axes[n + i].set_xlabel(r"$t$")
     axes[n + i].set_ylabel(labels_u[i])
 if closed_loop:
-    plt.savefig('cartpole_swingup_cl.png', bbox_inches='tight')
+    plt.savefig("cartpole_swingup_cl.png", bbox_inches="tight")
 else:
-    plt.savefig('cartpole_swingup_ol.png', bbox_inches='tight')
+    plt.savefig("cartpole_swingup_ol.png", bbox_inches="tight")
 plt.show()
 
 if animate:
     fig, ani = animate_cartpole(t, s[:, 0], s[:, 1])
-    ani.save('cartpole_swingup.mp4', writer='ffmpeg')
+    ani.save("cartpole_swingup.mp4", writer="ffmpeg")
     plt.show()
